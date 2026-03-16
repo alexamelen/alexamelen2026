382f4003597074a69f3a82799538df72758b4e1a
Alex Melendez <alexamelen.03@gmail.com>
2026-03-16 01:34:35 -0500

    adding responsiveness to landing and navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
-import { Link } from "react-router-dom";
-import "./Navbar.css";
+import './Navbar.css'
 
 const Navbar = () => {
+
   return (
-    <div className='nav-wrapper'>
-      <div className='logo'>
-      <p className = "logo-name" >Alex Melendez</p>
-      </div>
-      <div className='menu'>
-        <a href="#home" className="menu-item">home</a>
-        <a href="#about" className="menu-item">about</a>
-        <a href="#projects" className="menu-item">projects</a>
-      </div>
-      </div>
+    <div className="nav-wrapper">
+  <div className="logo">
+    <p className="logo-name">Alex Melendez</p>
+  </div>
+
+  <div className="hamburger">
+    ☰
+  </div>
+
+  <div className="menu">
+    <a href="#home" className="menu-item">home</a>
+    <a href="#about" className="menu-item">about</a>
+    <a href="#projects" className="menu-item">projects</a>
+  </div>
+</div>
   )
 }
 
-export default Navbar
+export default Navbar;
